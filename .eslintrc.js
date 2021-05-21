c1963c94fe9f740c277b351b70005f6c442ce135
Liam Li <398389303@qq.com>
2021-05-21 19:06:54 +0800

    fix eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,8 @@
-module.exports = {
+export default {
     env: {
         browser: true,
         es2021: true,
     },
-
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
